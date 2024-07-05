19298369612b81bfef540ef41cc4f54ecb0e3705
LahiruAvishka <wlavishka.95@gmail.com>
2024-07-05 23:09:29 +0530

    Two way data bained

---
+++ src/app/page/manage-emp/manage-emp.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-manage-emp',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './manage-emp.component.html',
+  styleUrl: './manage-emp.component.css'
+})
+export class ManageEmpComponent {
+
+  public firstName :string=""
+  addEmployee() {
+    alert(this.firstName)
+  }
+}
